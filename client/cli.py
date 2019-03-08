79f0b7b563458fe7029ed438f87c01e1687a901e
Marek Kochanowski <marek@kochanow.ski>
2019-03-08 19:02:10 +0100

    Update client/cli.py
    Co-Authored-By: zofiakochutek <33512122+zofiakochutek@users.noreply.github.com>

--- client/cli.py
+++ client/cli.py
@@ -24,7 +24,7 @@ def download(workspace, project, version, model):
 )
 @click.option("--project", prompt="Project name", help="Name of the model to upload")
 @click.option("--version", prompt="Model version", help="Version of the model to upload")
-@click.option("--model", prompt="Model filename", help="Filename of model to upload")
+@click.option("--model", prompt="Model filename", help="Filename of the model to upload")
 def upload(workspace, project, version, model):
     """Uploads a given model."""
     print("Uploading model...")
