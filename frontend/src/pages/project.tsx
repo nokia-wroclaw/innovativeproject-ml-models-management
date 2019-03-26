6a2908ab6f0d6c5435898a150fd003fccf6b4bbc
Clechay <luke.clechay@gmail.com>
2019-03-26 20:32:32 +0100

    improve getProject usage avoid changes on render

--- frontend/src/pages/project.tsx
+++ frontend/src/pages/project.tsx
@@ -15,61 +15,63 @@ const styles = (theme: Theme) =>
 	});
 
 type State = {
-	status:"loading"|"loaded"|"failed";
-	id?:string;
-	name?:string;
-	repoUrl?:string;
-	allParameters?:string[];
-	allHiperParameters?:string[];
+	status: "loading" | "loaded" | "failed";
+	id?: string;
+	name?: string;
+	repoUrl?: string;
+	allParameters?: string[];
+	allHiperParameters?: string[];
 };
 
 interface ProjectRouterProps {
 	projectId: string;   // This one is coming from the router
 }
 interface Props extends WithStyles<typeof styles>, RouteComponentProps<ProjectRouterProps> {
-	
+
 }
 class ProjectComponent extends React.Component<Props, State> {
-	lastReq:string = "";
+	lastReq: string = "";
 	constructor(props: Props) {
 		super(props);
 		this.state = {
-			status:"loading"
+			status: "loading"
 		}
 	}
-	getProject = async ()=>{
+	getProject = async () => {
 		const urlId = this.props.match.params.projectId;
-		this.setState({status:"loading"})
+		this.setState({ status: "loading" })
 		const project = await ProjectFetch.getProject(urlId);
 
-		if(!project.successful){
-			this.setState({status:"failed"});
+		if (!project.successful) {
+			this.setState({ status: "failed" });
 			return;
 		}
 
 		this.setState({
-			id:project.id,
-			name:project.name,
-			repoUrl:project.repoUrl,
-			allParameters:project.allParameters,
-			allHiperParameters:project.allHiperParameters,
-			status:"loaded"
+			id: project.id,
+			name: project.name,
+			repoUrl: project.repoUrl,
+			allParameters: project.allParameters,
+			allHiperParameters: project.allHiperParameters,
+			status: "loaded"
 		})
 	}
-	render() {
+	componentDidUpdate(prevProps: Props, prevState: State) {
 		const urlId = this.props.match.params.projectId;
-		if(urlId !== this.lastReq && urlId !== this.state.id){
-			this.lastReq = urlId;
-			this.getProject();
-		}
-
-		if(this.state.status === "loaded") return(
+		const prevUrlId = prevProps.match.params.projectId;
+		if (urlId !== prevUrlId) this.getProject();
+	}
+	componentDidMount() {
+		this.getProject();
+	}
+	render() {
+		if (this.state.status === "loaded") return (
 			<p>loaded</p>
 		)
-		else if(this.state.status === "loading") return(
+		else if (this.state.status === "loading") return (
 			<p>loading</p>
 		)
-		return(
+		return (
 			<p>failed</p>
 		)
 	}
