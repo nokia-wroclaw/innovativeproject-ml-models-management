eac9db3e58873099fa93eb6d918b2f8ae167e0de
Clechay <luke.clechay@gmail.com>
2019-03-26 20:11:10 +0100

    clean up Home

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -3,51 +3,51 @@ import {
 	Button, Dialog, DialogActions,
 	DialogContent, DialogContentText,
 	DialogTitle, Typography, Divider, TextField,
-	FormControl, FormControlLabel, FormLabel, Paper, Checkbox, Input, InputLabel, Avatar
+	FormControl, FormControlLabel, FormLabel, Paper, Checkbox, Input, InputLabel, Avatar, ExpansionPanel, ExpansionPanelSummary
 } from '@material-ui/core';
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import { createStyles, WithStyles } from '@material-ui/core/styles';
 import withStyles from '@material-ui/core/styles/withStyles';
-import { TransformRounded as Logo , LockOutlined as LockOutlinedIcon} from '@material-ui/icons';
+import { ExpandMore as ExpandMoreIcon } from '@material-ui/icons';
+
 
 const styles = (theme: Theme) =>
 	createStyles({
-		main: {
-			width: 'auto',
-			display: 'block', // Fix IE 11 issue.
-			marginLeft: theme.spacing.unit * 3,
-			marginRight: theme.spacing.unit * 3,
-			[theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-				width: 400,
-				marginLeft: 'auto',
-				marginRight: 'auto',
+		root: {
+			width: '100%',
+		},
+		heading: {
+			fontSize: theme.typography.pxToRem(15),
+		},
+		secondaryHeading: {
+			fontSize: theme.typography.pxToRem(15),
+			color: theme.palette.text.secondary,
+		},
+		icon: {
+			verticalAlign: 'bottom',
+			height: 20,
+			width: 20,
+		},
+		details: {
+			alignItems: 'center',
+		},
+		column: {
+			flexBasis: '33.33%',
+		},
+		helper: {
+			borderLeft: `2px solid ${theme.palette.divider}`,
+			padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
+		},
+		link: {
+			color: theme.palette.primary.main,
+			textDecoration: 'none',
+			'&:hover': {
+				textDecoration: 'underline',
 			},
 		},
-		paper: {
-			marginTop: theme.spacing.unit * 8,
-			display: 'flex',
-			flexDirection: 'column',
-			alignItems: 'center',
-			padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-		},
-		avatar: {
-			margin: theme.spacing.unit,
-			backgroundColor: theme.palette.secondary.main,
-		},
-		form: {
-			width: '100%', // Fix IE 11 issue.
-			marginTop: theme.spacing.unit,
-		},
-		submit: {
-			marginTop: theme.spacing.unit * 3,
-		},
-		logo:{
-			height:"7rem",
-			width:"auto",
-			marginTop:"1rem",
-			marginBottom:"2rem",
-		}
 	});
+
+
 
 type State = {
 };
@@ -58,7 +58,8 @@ class HomeC extends React.Component<WithStyles<typeof styles>, State> {
 		const { classes } = this.props;
 
 		return (
-			<main className={classes.main}>
+			<main>
+				
 			</main>
 		);
 	}
