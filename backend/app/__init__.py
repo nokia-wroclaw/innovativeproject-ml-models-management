34b5c438255a06494c977f96a4de86bc2f1017bd
Marek Kochanowski <marek@kochanow.ski>
2019-04-01 00:47:59 +0200

    Add basic storage management

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,10 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
+from flask_uploads import UploadSet, configure_uploads, ALL
+from flask_praetorian import Praetorian
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
 ma = Marshmallow()
+praetorian = Praetorian()
+cors = CORS()
+
+models_uploadset = UploadSet(name="models", extensions=ALL)
+
 
 def create_app(config_object: str = "Production") -> object:
     """Creates Flask app using one of provided configuration objects.
@@ -19,10 +27,18 @@ def create_app(config_object: str = "Production") -> object:
     db.init_app(app)
     migrate.init_app(app, db)
     ma.init_app(app)
+    cors.init_app(app)
+    configure_uploads(app, (models_uploadset,))
+
+    from app.models import User
+
+    praetorian.init_app(app, User)
 
     from app.api import api_bp
+    from app.storage import storage_bp
 
     app.register_blueprint(api_bp, url_prefix="/api/v1")
+    app.register_blueprint(storage_bp, url_prefix="/storage")
 
     return app
 
