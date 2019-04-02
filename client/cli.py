2f003f53787c026341b5d3d16946fc87e592c35a
Zofia Kochutek <zofiakochutek@gmail.com>
2019-04-02 10:42:22 +0200

    Extend cli

--- client/cli.py
+++ client/cli.py
@@ -1,4 +1,6 @@
-import click
+import click, requests
+
+url = "http://nokia-projekt:5000"
 
 
 @click.group()
@@ -15,7 +17,15 @@ def cli():
 @click.option("--model", prompt="Model filename", help="Filename of the selected model")
 def download(workspace, project, version, model):
     """Downloads requested model."""
-    print("Downloading model...")
+    # print("Downloading model...")
+    payload = {
+        "workspace": workspace,
+        "project": project,
+        "model": model,
+        "version": version,
+    }
+    r = requests.get(url, params=payload)
+    model = r.text
 
 
 @click.command()
@@ -23,24 +33,64 @@ def download(workspace, project, version, model):
     "--workspace", prompt="Workspace name", help="Name of the model to upload"
 )
 @click.option("--project", prompt="Project name", help="Name of the model to upload")
-@click.option("--version", prompt="Model version", help="Version of the model to upload")
-@click.option("--model", prompt="Model filename", help="Filename of the model to upload")
+@click.option(
+    "--version", prompt="Model version", help="Version of the model to upload"
+)
+@click.option(
+    "--model", prompt="Model filename", help="Filename of the model to upload"
+)
 def upload(workspace, project, version, model):
     """Uploads a given model."""
-    print("Uploading model...")
+    # print("Uploading model...")
+    payload = {
+        "workspace": workspace,
+        "project": project,
+        "model": model,
+        "version": version,
+    }
+    r = requests.post(url, data=payload)
 
 
 @click.command()
 # @click.argument('--list/--graph', default=False,
 # help="Lists all available workspaces")
-def workspaces():
-    """Lists available workspaces."""
-    print("Listing workspaces...")
+def graph():
+    r = requests.get(url)
+    r_dict = r.json()
+    click.echo(r.dict["form"])
+
+
+@click.command()
+@click.option(
+    "--workspace",
+    prompt="Workspace name",
+    help="Name of the workspace, which projects will be listed",
+)
+def projects():
+    payload = {"project": workspace}
+    r = requests.get(url, params=payload)
+    r_dict = r.json()
+    click.echo(f"Projects of workspace: {workspace}: \n {r.dict['form']}")
+
+
+@click.command()
+@click.option(
+    "--project",
+    prompt="Project name",
+    help="Name of the project, which models will be listed",
+)
+def models():
+    payload = {"project": project}
+    r = requests.get(url, params=payload)
+    r_dict = r.json()
+    click.echo(f"Models of project {project}: \n {r.dict['form']}")
 
 
 cli.add_command(upload)
 cli.add_command(download)
-cli.add_command(workspaces)
+cli.add_command(projects)
+cli.add_command(models)
+cli.add_command(graph)
 
 if __name__ == "__main__":
     cli()
