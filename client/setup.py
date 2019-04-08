49eb2fc0365a91c246939c00c2ae13a9e6aaf2b0
Marek Kochanowski <marek@kochanow.ski>
2019-04-08 20:38:34 +0200

    Replace CLI executable entrypoint

--- client/setup.py
+++ client/setup.py
@@ -6,5 +6,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=["Click"],
-    entry_points={"console_scripts": ["mlmm = mlmm.cli:cli"]},
+    entry_points={"console_scripts": ["mlmm = mlmm.interface:cli"]},
 )
