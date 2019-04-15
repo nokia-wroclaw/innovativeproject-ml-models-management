c523b9158003d2bb1ae7cb7311661f6e99872b1b
Zofia Kochutek <zofiakochutek@gmail.com>
2019-04-15 21:14:21 +0200

    Implement Projects: add, ls

--- client/maisie/cli/projects.py
+++ client/maisie/cli/projects.py
@@ -1,25 +1,90 @@
 import click
-
+import logging
 from terminaltables import SingleTable
 
 from maisie import Projects
 from maisie.utils.misc import Transform
 
+DEFAULT_DISPLAY_ATTRIBUTES = [
+    "id",
+    # "workspace_id",
+    "name",
+    "description",
+    "git_url",
+    "created",
+]
+
+
 @click.group()
 def projects():
     pass
 
+
 @click.command()
-def add():
-    pass
+@click.option(
+    "-n", "--name", prompt="Project's name", help="Name of the project to add"
+)
+@click.option(
+    "-d",
+    "--description",
+    prompt="Project's description",
+    help="Description of the project to add",
+)
+@click.option(
+    "-git", "--git_url", prompt="Git Repository (URL)", help="Url of git repository"
+)
+def add(name, description, git_url):
+    project = Projects()
+    project = project.create(name, description, git_url)
+    table = SingleTable(
+        Transform().api_response_to_terminaltables(
+            project, include=DEFAULT_DISPLAY_ATTRIBUTES
+        )
+    )
+    table.inner_row_border = True
+    table.title = "List of projects"
+    click.echo(table.table)
+
 
 @click.command()
 def rm():
     pass
 
+
 @click.command()
-def ls():
-    pass
+@click.option(
+    "-id", "--id", default=None, type=int, help="Returns project with a specified id"
+)
+def ls(id):
+    projects = Projects().get_all()
+
+    table = SingleTable(
+        Transform().api_response_to_terminaltables(
+            projects, include=DEFAULT_DISPLAY_ATTRIBUTES
+        )
+    )
+    table.inner_row_border = True
+    table.title = "List of projects"
+    click.echo(table.table)
+
+
+#     if id:
+#         projects = Projects().get(id)
+#         include = [
+#             "id"
+#         ]
+#     else:
+#         projects = Projects().get_all()
+#         include = DEFAULT_DISPLAY_ATTRIBUTES
+#     if projects:
+#         table = SingleTable(
+#             Transform().api_response_to_terminaltables(
+#                 projects, include=DEFAULT_DISPLAY_ATTRIBUTES
+#             )
+#         )
+#         table.inner_row_border = True
+#         table.title = "Most recently uploaded models"
+#         click.echo(table.table)
 
 
 projects.add_command(add)
--- client/maisie/resources/projects.py
+++ client/maisie/resources/projects.py
@@ -1,4 +1,11 @@
+import json
+import logging
+
+from typing import Union
 from maisie import BaseAction
+from maisie.utils.git import GitProvider
+
+logger = logging.getLogger(__name__)
 
 
 class Projects(BaseAction):
@@ -6,20 +13,49 @@ class Projects(BaseAction):
     def selected(self):
         return self.config.selected_project
 
-    def create(self):
-        pass
+    def create(self, name: str, description: str, git_url: str):
+        results = []
+        with self.config.session as session:
+            payload = {
+                "name": name,
+                "description": description,
+                "git_url": git_url,
+                "workspace_id": self.config.selected_workspace,
+            }
+
+            request = session.post(f"{self.config.api_url}/projects/", data=payload)
+
+            if "data" in request.json():
+                results.append(request.json()["data"])
+            else:
+                logger.error("Could not create a project.")
+
+        return results
 
     def update(self, id: int, data: dict):
         pass
 
-    def detele(self, id: int):
+    def delete(self, id: int):
         pass
 
     def get(self, id: int):
         pass
 
     def get_all(self, page=None, per_page=None):
+        results = []
         if not page:
             page = self.config.api_page
         if not per_page:
             per_page = self.config.api_per_page
+        with self.config.session as session:
+            request = session.get(
+                f"{self.config.api_url}/projects/",
+                params={"page": page, "per_page": per_page},
+            )
+            if "data" in request.json():
+                results = request.json()["data"]
+                logger.debug(f"Response body: {results}")
+            else:
+                logger.error("Could not fetch any projects.")
+
+        return results
